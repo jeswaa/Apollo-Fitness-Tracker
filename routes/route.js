285c2e0b84b526c83a38962fc3e9f7710efae397
Rinderuu <lhyndelsario94@gmail.com>
2024-10-15 02:05:46 +0800

    ratbu

--- routes/route.js
+++ routes/route.js
@@ -14,14 +14,11 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup');  // This renders 'views/signup.ejs'
 });
-<<<<<<< HEAD
-=======
+
 
 router.get('/guest', (req, res) => {
   res.render('guest');  // This renders 'views/geust-page.ejs'
 });
 
 //buratburat
-
->>>>>>> 2eeb7e5b7e911f0989b8cb1a66790012f92bd237
 export default router;
